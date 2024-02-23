68c786d24587ef9c6857b19dc58d23f3adfee4f5
Uros Rajkovic <urosrajkovic011@hotmail.com>
2024-02-23 01:49:12 +0100

    sort travels

--- Jungle/wwwroot/js/site.js
+++ Jungle/wwwroot/js/site.js
@@ -11,11 +11,42 @@ $(document).ready(function () {
         var selectedValue = $(this).val();
 
         // Do something with the selected value
-        alert("Selected value:" + selectedValue);
+       // alert("Selected value:" + selectedValue);
         if (selectedValue == "Alphabetically") {
-            var tripRow = $(".trip-list");
-            var tripRowContryName = $(".trip-list .countryName");
-            console.log(tripRowContryName);
+            var tripRowContryName = $(".countryName");
+            let parents = tripRowContryName.parent().parent().parent();
+            $.each(tripRowContryName, function (index) {
+                let names = $(this).html();
+                let uniqueId = names.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(''); // Creating a unique ID using the index
+                parents.eq(index).attr('id', uniqueId); // Setting ID for the corresponding parent element
+                //remove all div elements
+                $(".trip-list").css("display", "none");
+            })
+            var idsArray = [];
+            // Sort the array alphabetically
+            // Iterate over each element with class 'trip-list'
+            $('.trip-list').each(function () {
+                // Get the id of the current element and push it to the array
+                idsArray.push($(this).attr('id'));
+            });
+
+            // Now, idsArray contains the ids of all the divs with class 'trip-list'
+            console.log(idsArray);
+            idsArray.sort();
+            // Detach all divs with class 'trip-list'
+            var $tripLists = $('.trip-list').detach();
+            // Append divs in sorted order
+            idsArray.forEach(function (id) {
+                // Find div element by id
+                var $currentDiv = $tripLists.filter('#' + id);
+
+                // Append the div to its sorted position
+                $currentDiv.appendTo('.trip-list-wrapper');
+            });
+
+            // Apply display: flex to all divs with class 'trip-list'
+            $('.trip-list').css('display', 'flex');
+               
         }
     });
 });
